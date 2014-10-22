34d6a023fb64c599bd4ac56cbbced7dcdeceac74
Richard Zhan <lilfirebolt@gmail.com>
2014-10-22 13:10:58 -0400

    Comments working? Maybe

--- app.py
+++ app.py
@@ -33,15 +33,28 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route('/title/<title>')
+@app.route('/title/<title>', methods=["POST","GET"])
 def individual_title(title):
     conn = sqlite3.connect('test.db')
     c = conn.cursor()
-
+    if request.method == "POST":
+        form = request.form
+        if form['submit'] == 'yes' and form['comment']!='':
+            
+            comment = form['comment']
+            f=open('comment.csv','a')
+            now = datetime.datetime.now()
+            f.write("%s,%s,%d,%d,%d,%d,%d\n"%(title,comment,now.month,now.day,now.year,now.hour,now.minute))
+            c.execute("INSERT INTO comments VALUES ('%s','%s',%d,%d,%d,%d,%d);"%(title,comment,now.month,now.day,now.year,now.hour,now.minute))
+            conn.commit()
+            f.close()
     query = "SELECT * FROM blogs WHERE title =\'" + title +"\'"
     q=[x for x in c.execute(query)]
-    print q
-    return render_template("title.html", titles = q)
+    #print q
+    query = "SELECT * FROM comments WHERE title =\'" + title +"\'"
+    q2=[x for x in c.execute(query)]
+    #print q2
+    return render_template("title.html", titles = q, comments = q2)
 
 if __name__ == '__main__':
     app.debug = True
--- create.py
+++ create.py
@@ -5,17 +5,29 @@ def creat():
     q1=""
     try:
         c.execute("create table blogs (title text UNIQUE, post text, month integer, day integer, year integer, hour integer, minute integer)")
-        
+        c.execute("create table comments (title text, comment text, month integer, day integer, year integer, hour integer, minute integer)")
     except:
         c.execute("drop table blogs")
+        c.execute("drop table comments")
         c.execute("create table blogs (title text UNIQUE, post text, month integer, day integer, year integer, hour integer, minute integer)")
+        c.execute("create table comments (title text, comment text, month integer, day integer, year integer, hour integer, minute integer)")
         
     conn.commit()
     f=open('content.csv').readlines()
     q = "INSERT INTO blogs VALUES(%(title)s,%(post)s,%(month)s,%(day)s,%(year)s,%(hour)s,%(minute)s)"
     
     for x in csv.DictReader(open('content.csv')):
-        print "I WORK"
+        #print "I WORK"
+        q1 = q%x
+        c.execute(q1)
+    conn.commit()
+
+    
+    f=open('comment.csv').readlines()
+    q = "INSERT INTO comments VALUES(%(title)s,%(comment)s,%(month)s,%(day)s,%(year)s,%(hour)s,%(minute)s)"
+    
+    for x in csv.DictReader(open('comment.csv')):
+        #print "I WORK"
         q1 = q%x
         c.execute(q1)
     conn.commit()
