148608d9a80fe54de54cc4b2aed76c86dccb5934
d-tsui <dtsui918@yahoo.com>
2014-10-22 22:20:03 -0400

    final push

--- app.py
+++ app.py
@@ -24,10 +24,18 @@ def index():
     query = "SELECT * FROM blogs"
 
     q=[x for x in c.execute(query)]
-    print q
-    conn.commit()
+    #print q
 
-    return render_template("index.html", titles =reversed(q))
+    #to get the number of comments
+    q2 = {}
+    for x in reversed(q):
+        query = "SELECT title FROM comments WHERE title =\'" + x[0] +"\'"
+        qtemp = [y for y in c.execute(query)]
+        q2[x[0]] = len(qtemp)
+    conn.commit()
+    print q2
+
+    return render_template("index.html", titles =reversed(q),lengths = q2)
 
 @app.route("/about")
 def about():
@@ -40,7 +48,7 @@ def individual_title(title):
     if request.method == "POST":
         form = request.form
         if form['submit'] == 'yes' and form['comment']!='':
-            
+
             comment = form['comment']
             f=open('comment.csv','a')
             now = datetime.datetime.now()
@@ -54,7 +62,7 @@ def individual_title(title):
     query = "SELECT * FROM comments WHERE title =\'" + title +"\'"
     q2=[x for x in c.execute(query)]
     #print q2
-    return render_template("title.html", titles = q, comments = q2)
+    return render_template("title.html", titles = q, comments = reversed(q2))
 
 if __name__ == '__main__':
     app.debug = True
